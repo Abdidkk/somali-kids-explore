a5ed9cfe650fc9a20493e1e63108da65499e515e
Abdidkk <Abdi.dk1@gmail.com>
2025-12-20 10:49:55 +0100

    .......

--- src/constants/userGuideData.ts
+++ src/constants/userGuideData.ts
@@ -5,8 +5,12 @@ export interface GuideItem {
   }
   
   export const USER_GUIDE_DATA: GuideItem[] = [
-    { id: 'delete-account', video: "/Video/Dugsi.mp4", description: 'Hvordan sletter jeg min konto?' },
+
+    { id: 'start-learning', video: "/Video/Dugsi.mp4", description: 'Sådan starter du læring?' },
+    { id: 'child-switching', video: "/Video/Dugsi.mp4", description: 'Sådan skifter du mellem børn?' },
+    { id: 'category-control', video: "/Video/Dugsi.mp4", description: 'Sådan bruger du Kategori Kontrol?' },
     { id: 'reset-data', video: '', description: 'Hvordan nulstiller jeg data?' },
+    { id: 'delete-account', video: "/Video/Dugsi.mp4", description: 'Hvordan sletter jeg min konto?' },
     { id: 'manage-children', video: '', description: 'Hvordan administrerer jeg børneprofiler?' },
     { id: 'start-learning', video: '', description: 'Kom godt i gang med læring' }
   ];
